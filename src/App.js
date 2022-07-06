6d009c21fc34da31d95cc2a99daeb4e87ea78f19
nasser-gamal <98104328+nasser-gamal@users.noreply.github.com>
2022-07-06 22:49:07 +0200

    Upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./Components/Homee/Home";
-import Landing from "./Components/Landing/Landing";
 import SearchPage from "./Components/SearchPage/SearchPage";
 import Navbar from "./Components/NavBar/Navbar";
 import Register from "./Components/Form/Register/Register";
-import { useEffect } from "react";
 import Cart from "./Components/Cart/Cart";
 import Login from "./Components/Form/Login/Login";
 import Product from "./Components/Product/Product";
 import Footer from "./Components/Footer/Footer";
-import ScrollToTop from "./Components/ScrollToTop";
 
 
 const App = () => {
